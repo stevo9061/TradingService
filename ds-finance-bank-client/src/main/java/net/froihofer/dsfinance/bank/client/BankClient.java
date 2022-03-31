b9b2432050b3861c10c29b8ad964af3a7f714baf
stevo9061 <sbittgen1@gmail.com>
2022-03-31 11:20:36 +0200

    - [FIXED]: Successfully connected from Client to the Webservices. - [FIXED]: It is possible to search and buy stocks.
    - [TODO]: Create the Service (EJB) on server side, and just login with a client or employee to use this services.

--- ds-finance-bank-client/src/main/java/net/froihofer/dsfinance/bank/client/BankClient.java
+++ ds-finance-bank-client/src/main/java/net/froihofer/dsfinance/bank/client/BankClient.java
@@ -1,15 +1,27 @@
 package net.froihofer.dsfinance.bank.client;
 
-import io.codejournal.maven.wsdl2java.FindStockQuotesByCompanyName;
+
+import net.froihofer.dsfinance.ws.trading.PublicStockQuote;
+import net.froihofer.dsfinance.ws.trading.TradingWSException_Exception;
+import net.froihofer.dsfinance.ws.trading.TradingWebService;
+import net.froihofer.dsfinance.ws.trading.TradingWebServiceService;
+
+
 import net.froihofer.util.AuthCallbackHandler;
 import net.froihofer.util.JBoss7JndiLookupHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.xml.ws.BindingProvider;
+import java.math.BigDecimal;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+import java.util.Scanner;
 
 /**
  * Main class for starting the bank client.
@@ -18,29 +30,88 @@ import java.util.Properties;
 
 public class BankClient {
   private static Logger log = LoggerFactory.getLogger(BankClient.class);
-  
+
   public static void main(String[] args) {
 
-    AuthCallbackHandler.setUsername("customer");
+
+/*    AuthCallbackHandler.setUsername("customer");
     AuthCallbackHandler.setPassword("customerpass");
-//    AuthCallbackHandler.setUsername("Balou");
-//    AuthCallbackHandler.setPassword("1234");
-
-
     Properties props = new Properties();
     props.put(Context.SECURITY_PRINCIPAL,AuthCallbackHandler.getUsername());
-    props.put(Context.SECURITY_CREDENTIALS,AuthCallbackHandler.getPassword());
+    props.put(Context.SECURITY_CREDENTIALS,AuthCallbackHandler.getPassword());*/
+
+    Properties props = new Properties();
+
     try {
       JBoss7JndiLookupHelper jndiHelper = new JBoss7JndiLookupHelper(new InitialContext(props), "ds-finance-bank-ear", "ds-finance-bank-ejb", "");
       //TODO: Implement the rest of the functionality
 
-//      FindStockQuotesByCompanyName Apple = new FindStockQuotesByCompanyName();
-//
-//      Apple.setPartOfCompanyName("Apple");
-//      Apple.getPartOfCompanyName();
+
+      TradingWebServiceService tradingWebServiceService = new TradingWebServiceService(new URL(
+              "http://edu.dedisys.org/ds-finance/ws/TradingService?wsdl"));
+
+              TradingWebService proxy = tradingWebServiceService.getTradingWebServicePort();
+
+      BindingProvider bindingProvider = (BindingProvider) proxy;
+
+      bindingProvider.getRequestContext().put(
+              BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+              "http://edu.dedisys.org/ds-finance/ws/TradingService");
+
+              bindingProvider.getRequestContext().put(
+                      BindingProvider.USERNAME_PROPERTY, "bic4a22_04");
+
+      bindingProvider.getRequestContext().put(
+              BindingProvider.PASSWORD_PROPERTY, "IoD6eic");
+      System.out.println("");
+      System.out.println("###>>>> Client has connected to the Server successfully! <<<<###");
+
+
+
+      System.out.println("Which company do you want to search for?");
+      Scanner scanName = new Scanner(System.in);
+      String companyName = scanName.nextLine();
+
+
+      List<PublicStockQuote> client = proxy.findStockQuotesByCompanyName(companyName);
+
+
+      for(PublicStockQuote publicStockQuote: client) {
+        System.out.println("Company name: " + publicStockQuote.getCompanyName());
+        System.out.println("Symbol (You need this to buy the stock): " + publicStockQuote.getSymbol());
+        System.out.println("Trading Price: " + publicStockQuote.getLastTradePrice());
+        System.out.println();
+      }
+
+
+      System.out.println("Which stock do you want to buy, type in the symbol:");
+      Scanner symbolName = new Scanner(System.in);
+      String symbol = symbolName.nextLine();
+
+      BigDecimal lastPriceStock = null;
+      for(PublicStockQuote iterator : client) {
+        if(iterator.getSymbol().equals(symbol)) {
+          System.out.println();
+          System.out.println("The trade price from this stock is: " + iterator.getLastTradePrice() + "€");
+          System.out.println();
+          lastPriceStock = iterator.getLastTradePrice();
+        break;
+        }
+      }
+//      System.out.println(client.get(0).getSymbol());
+
+      System.out.println("How often are you going to buy the stock?");
+      Scanner scannerStockCount = new Scanner(System.in);
+      int stockCount = scannerStockCount.nextInt();
+      proxy.buy(symbol, stockCount);
+
+      System.out.println("You have successfully bought " + stockCount + "x shares of the stock at a price of " + lastPriceStock + "€");
+
+
+
 
     }
-    catch (NamingException e) {
+    catch (NamingException | MalformedURLException | TradingWSException_Exception e) {
       log.error("Failed to initialize InitialContext.",e);
     }
   }
